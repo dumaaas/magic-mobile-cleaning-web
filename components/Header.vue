93323e5148bab443f9cc4c68f37169dc57d60709
Marko Dumnic <markodumnic8@gmail.com>
2021-09-25 19:07:27 +0200

    Added hamburger menu

--- components/Header.vue
+++ components/Header.vue
@@ -24,7 +24,7 @@
         </ul>
       </div>
       <div class="header-main__mobile mobile">
-        <div class="header-main__mobile-hamburger">
+        <div class="header-main__mobile-hamburger" @click="opened = true">
           <div></div>
           <div></div>
           <div></div>
@@ -33,6 +33,23 @@
           <Logo />
         </div>
         <div></div>
+      </div>
+      <div class="header-menu mobile" :class="{ opened: opened }">
+        <div class="hamburger-close" @click="opened = false">
+          <div></div>
+          <div></div>
+        </div>
+        <div class="header-menu__links">
+          <ul>
+            <li
+              @click="opened = false"
+              v-for="link in mainLinks"
+              :key="link.name"
+            >
+              <nuxt-link :to="`${link.url}`">{{ link.name }}</nuxt-link>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </header>
@@ -69,6 +86,7 @@ export default {
           url: "/contact",
         },
       ],
+      opened: false,
     };
   },
 };
@@ -78,9 +96,17 @@ export default {
 @import "@/assets/sass/abstracts/_colors.scss";
 @import "@/assets/sass/abstracts/_mixins.scss";
 
+.mobile {
+  display: none !important;
+  @include breakpoint($xs) {
+    display: flex !important;
+  }
+}
 .header {
   font-weight: 600;
   background: $secondary;
+  position: relative;
+  z-index: 10;
   ul {
     display: flex;
     gap: 2.5rem;
@@ -132,6 +158,7 @@ export default {
     }
   }
   &-main {
+    position: relative;
     display: flex;
     justify-content: center;
     background-color: $primary-darker;
@@ -170,7 +197,17 @@ export default {
         display: flex;
       }
       &-hamburger {
+        cursor: pointer;
+        &:hover {
+          div {
+            transition: all 0.3s ease-in;
+
+            background: $primary;
+          }
+        }
         div {
+          transition: all 0.3s ease-out;
+
           width: 34px;
           height: 5px;
           margin-top: 5px;
@@ -180,6 +217,81 @@ export default {
             margin-top: 0px;
           }
         }
+      }
+    }
+  }
+  &-menu {
+    position: fixed;
+    overflow: hidden;
+    z-index: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    transform-origin: left top;
+    transform: translateY(calc(-100vh));
+    width: 100vw;
+    background: rgba(11, 64, 81, 0.95);
+    backdrop-filter: blur(5px);
+    transition: all 0.5s ease-out;
+    .hamburger-close {
+      position: absolute;
+      cursor: pointer;
+      top: 10px;
+      left: 20px;
+      height: 44px;
+      width: 44px;
+      transition: all 0.3s ease-out;
+      &:hover {
+        background: $primary;
+        transition: all 0.3s ease-in;
+        div {
+          background: $primary-darker;
+        }
+      }
+      div {
+        width: 34px;
+        height: 5px;
+        border-radius: 6px;
+        background: $secondary;
+        margin-top: 20px;
+        margin-left: 5px;
+        transition: all 0.3s ease-out;
+
+        &:first-child {
+          transform: rotate(45deg);
+        }
+        &:nth-child(2) {
+          transform: rotate(-45deg);
+          margin-top: -4px;
+        }
+      }
+    }
+    &.opened {
+      transform: translateY(0);
+      transition: all 0.5s ease-in;
+    }
+    &__links {
+      display: flex;
+      align-items: flex-end;
+      padding: 19px 0 21px 0;
+
+      ul {
+        margin-top: -72px;
+        flex-direction: column;
+        text-align: center;
+      }
+      a {
+        font-weight: 600;
+        font-size: 1.125rem;
+        letter-spacing: 0.081rem;
+        padding-bottom: 10px;
+      }
+
+      a:hover {
+        border-bottom: 4px solid $primary;
+        color: $primary;
+        padding-bottom: 10px;
       }
     }
   }
